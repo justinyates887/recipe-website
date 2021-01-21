41e26a264a4a537b6bc4a17c2293a9788c9bc8d3
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-01-20 21:41:44 -0600

    added carousel placeholder

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,30 @@
 //Scripts
+M.AutoInit();
+$('.chips').chips();
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+
+var chipsData = M.Chips.getInstance($('.chips')).chipsData;
+var chipsDataJson = JSON.stringify(chipsData); 
+
+
+  
+$("#search").on("click", function(event) {
+  event.preventDefault();
+
+  // Puts data from chips into objects in an array
+  var instance = M.Chips.getInstance($(".chips"));
+  var ingredientEntered = instance.chipsData;
+  
+  // Alerts user if too many ingredients are entered. Edamam API only allows up to 5 ingredients. Any suggestions on a better way to do this are welcome!
+  if (ingredientEntered.length <= 4) {
+    console.log(ingredientEntered)
+  } else {
+    alert("Maximum 5 ingredients")
+  }
+
+});

bdff71927b0d3110e8d61452e862652a9e0f459d
Justin Yates <justinyates887@gmail.com>
2021-01-20 17:50:44 -0800

    wrote mockup of unsplash API

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,3 +12,21 @@ const tags = document.querySelectorAll(".chips");
     M.Chips.init(tag_holder, {
       placeholder: "Enter ingredients"
     })
+
+    /*******************************************************Unsplash API ************************************************************************/
+    /*
+    const unsplashKey = 'IhPp4I1zglZsS5HfUmls4CF3gKJxuuBPeKVPwWEQmeo';
+    let recipe = $(``).val();
+    let queryURL = `https://api.unsplash.com/photos/?query=${recipe}&client_id=${unsplashKey}`;
+    const carousel = document.querySelector('');
+
+    async function getRecipeImages() {
+      return fetch(queryURL)
+        .then((response) => response.json())
+        .then((data) => {
+          let image1 = data.results[0];
+          return image.urls.regular;
+        })
+    }
+    */
+    /********************************************************************************************************************************************/
